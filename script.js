378db059ecb7b2c1ec5a8e1cdc4b67e5972e1508
João Pedro Vieira De Mello <joaomello1618@gmail.com>
2025-05-05 19:51:42 -0300

    att

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-const tarefas = ['Estudar HTML', 'Estudar CSS', 'Estudar JS']
+let tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
+function salvar(){
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+  }
 
-function add() {
+function add(){
     const input = document.querySelector('input')
     const tarefa = input.value
+    if (tarefa.trim() === '') return
     tarefas.push(tarefa)
-    input.value = ''
+    salvar()
     render()
+    input.value =''
 }
 
-function render() {
+function render(){
     const ul = document.querySelector('ul')
     ul.innerHTML = null
-    tarefas.forEach(function (tarefa) {
+    
+    tarefas.forEach(function(t){
         const li = document.createElement('li')
-        li.innerText = tarefa
-        console.log(tarefa)
+        li.innerText = t
         ul.appendChild(li)
-
     })
+    
+    
+}
+
+function deleAll(){
+    localStorage.clear()
+    location.reload(true)
 }
 render()
+
