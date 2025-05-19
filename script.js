b5ad09c9ba1292af4655c764e87c49f40b171aa8
João Pedro Vieira De Mello <joaomello1618@gmail.com>
2025-05-19 16:27:18 -0300

    att

--- script.js
+++ script.js
@@ -1,41 +1,50 @@
 const tarefas = []
 
-function add(){
+function add() {
     const input = document.querySelector('input')
     const tarefa = input.value
-    if(tarefa.trim() === '')return
-    tarefas.push(tarefa)
-    console.log(tarefas)
+    if (tarefa.trim() === '') return
+    tarefas.push({ texto: tarefa, feito: false })
     input.value = ''
     render()
 }
 
-function remove(index){
-    tarefas.splice(index , 1)
+function remove(index) {
+    tarefas.splice(index, 1)
     render()
 }
 
-function render(){
+function atualizarEstilo(span, feito){
+    span.style.textDecoration = feito ? 'line-through' : 'none'
+    span.style.opacity = feito ? '0.4' : '1'
+}
+
+
+function render() {
     const ul = document.querySelector('ul')
     ul.innerHTML = null
-    tarefas.forEach(function(t, index){
+    tarefas.forEach(function (t, index) {
         const li = document.createElement('li')
         const section = document.createElement('section')
-        
-        const checkbox = document.createElement('input')
-        checkbox.className = 'input-2'
-        checkbox.type = 'checkbox'
-        checkbox.onchange = function(){
-            span.style.textDecoration =  checkbox.checked ? 'line-through' : 'none'
-            span.style.opacity = checkbox.checked ? '0.4' : '1'
-        }
 
         const span = document.createElement('span')
-        span.innerText = t
-        
+        span.innerText = t.texto
+        atualizarEstilo(span, t.feito)
+
+        const checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
+        checkbox.className = 'input-2'
+        checkbox.checked = t.feito
+        checkbox.onchange = function () {
+            t.feito = checkbox.checked
+            atualizarEstilo(span, t.feito)
+        }
+
+
         const button = document.createElement('button')
-        button.innerHTML = 'Excluir'
-        button.onclick = function(){
+        button.innerHTML = '<span class="material-symbols-outlined">delete</span>'
+        button.className = 'botao-2'
+        button.onclick = function () {
             remove(index)
         }
         li.appendChild(section)
